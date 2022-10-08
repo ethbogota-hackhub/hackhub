64b8d3c249813e33933337983a1e9fcb12983395
Manuel <manuelozada11@hotmail.com>
2022-10-07 20:52:59 -0500

    add some basic resources

---
+++ src/common/utils.js
@@ -0,0 +1,46 @@
+export const scrollToTop = (value) => window.scrollTo({ behavior: 'smooth', top: value})
+
+export const removeItemStorage = (key) => {
+    sessionStorage.removeItem(key);
+}
+
+export const setStorageValue = (key, value) => {
+    sessionStorage.setItem(key, JSON.stringify(value));
+}
+
+export const getStorageValue = (key) => {
+    if (!key) return null;
+
+    const data = sessionStorage.getItem(key);
+
+    if (!data) return null;
+    
+    return JSON.parse(data);
+}
+
+export const firstLetterUppercase = (value) => {
+    if (!value) return '';
+
+    const str = value.toString();
+    return `${ str.substr(0,1).toUpperCase() }${ str.substr(1, str.length).toLowerCase() }`
+}
+
+export const dateFormat = (date) => {
+    return `${new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: "medium" }).format(date)}`;
+}
+
+export const orderBy = (array, by) => {
+    if (!array) return [];
+
+    if (by === 'date') 
+        return array.sort((a, b) => {
+            if (new Date(a.date).getTime() > new Date(b.date).getTime()) return -1;
+            else if (new Date(a.date).getTime() < new Date(b.date).getTime()) return 1;
+
+            return 0
+        });
+}
+
+export const round = (value, decimals) => {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
