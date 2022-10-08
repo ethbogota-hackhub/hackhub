57b7b3a3831b87888ccadca55a33d225f60f9a86
Manuel <manuelozada11@hotmail.com>
2022-10-08 04:10:57 -0500

    final visual when user add or update skills

--- src/views/UserSkills.jsx
+++ src/views/UserSkills.jsx
@@ -21,6 +21,7 @@ const UserSkills = () => {
         ]
     });
     const [transError, setTransError] = useState('');
+    const [update, setUpdate] = useState(false);
 
     const [option1,] = useState([
         "Select...",
@@ -125,7 +126,6 @@ const UserSkills = () => {
 
     const validateInputs = (data) => {
         let error = false;
-        console.log(data)
 
         if (data.attendeeAs === 'Select...') {
             setError('attendeeAs', null, { shouldFocus: true });
@@ -151,20 +151,25 @@ const UserSkills = () => {
             setError('userDescription', null, { shouldFocus: true });
             error = true;
         }
-        console.log(error)
 
         return error;
     }
 
-    const onUpdateData = (data) => {
+    const onSubmit = (data) => {
         try {
-            // console.log(data)
-            // if (validateInputs(data)) return ;
-            console.log(data);
+            if (validateInputs(data)) return ;
+
+            console.log('updating data');
+            setUpdate(false);
         } catch (e) {
             console.log(data);
             setTransError('Something went wrong');
         }
+    }
+
+    const onCancel = () => {
+        clearErrors();
+        setUpdate(false);
     }
 
     return (
@@ -180,13 +185,15 @@ const UserSkills = () => {
                 </div>
             </div>
 
-            <form onSubmit={ handleSubmit((e) => console.log(e)) }
+            <form onSubmit={ handleSubmit(onSubmit) }
                 className="container-sm border p-5 profile-container">
                 <h4 className="fw-bold">About your personal info...</h4>
 
                 <div className="pb-3">
                     <p className="mb-1"><span className="text-danger">*</span> How would you describe yourself?</p>
-                    <select type="select"
+                    <select name="option1"
+                        disabled={ !update }
+                        type="select"
                         {...register('userDescription', { required: true })} 
                         className={ `form-select ${ errors.userDescription ? 'is-invalid' : '' }` }>
                         { option1.map((opt, index) => <option key={ `${opt}${index}`} value={ opt }>{ opt }</option>) }
@@ -195,8 +202,10 @@ const UserSkills = () => {
 
                 <div className="border-bottom pt-3 pb-5">
                     <p className="mb-1"><span className="text-danger">*</span> Spoken Language</p>
-                    <select type="select"
-                        {...register('spokenLanguage', { required: true })} className={ `form-select ${ errors.spokenLanguage ? 'is-invalid' : ''  }` } name="option2">
+                    <select name="option2"
+                        disabled={ !update }
+                        type="select"
+                        {...register('spokenLanguage', { required: true })} className={ `form-select ${ errors.spokenLanguage ? 'is-invalid' : ''  }` }>
                         { option5.map((opt, index) => <option key={ `${opt}${index}`} value={ opt }>{ opt }</option>) }
                     </select>
                 </div>
@@ -205,31 +214,46 @@ const UserSkills = () => {
 
                 <div className="pb-3 ">
                     <p className="mb-1"><span className="text-danger">*</span> How do you plan to get involved?</p>
-                    <select type="select"
-                        {...register('attendeeAs', { required: true })} className={ `form-select ${ errors.attendeeAs ? 'is-invalid' : ''  }` } name="option3">
+                    <select name="option3"
+                        type="select"
+                        disabled={ !update }
+                        {...register('attendeeAs', { required: true })} 
+                        className={ `form-select ${ errors.attendeeAs ? 'is-invalid' : ''  }` }>
                         {  option2.map((opt, index) => <option key={ `${opt}${index}`} value={ opt }>{ opt }</option>) }
                     </select>
                 </div>
 
                 <div className="py-3">
                     <p className="mb-1"><span className="text-danger">*</span> Expertise with Ethereum</p>
-                    <select type="select"
-                        {...register('ethExperience', { required: true })} className={ `form-select ${ errors.ethExperience ? 'is-invalid' : ''  }` } name="option4">
+                    <select
+                        type="select"
+                        disabled={ !update }
+                        {...register('ethExperience', { required: true })} 
+                        className={ `form-select ${ errors.ethExperience ? 'is-invalid' : ''  }` }>
                         {  option3.map((opt, index) => <option key={ `${opt}${index}`} value={ opt }>{ opt }</option>) }
                     </select>
                 </div>
                 
                 <div className="pt-3">
                     <p className="mb-1"><span className="text-danger">*</span> Programming Languages</p>
-                    <select type="select"
-                        {...register('mainLanguage', { required: true })} className={ `form-select ${ errors.mainLanguage ? 'is-invalid' : ''  }` } name="option5">
+                    <select name="option5"
+                        disabled={ !update }
+                        type="select"
+                        {...register('mainLanguage', { required: true })} 
+                        className={ `form-select ${ errors.mainLanguage ? 'is-invalid' : ''  }` }>
                         {  option4.map((opt, index) => <option key={ `${opt}${index}`} value={ opt }>{ opt }</option>) }
                     </select>
                 </div>
 
                 <div className="pt-4 d-flex justify-content-center align-items-center">
-                    <button className="btn btn-outline-primary m-2" onClick={() => { clearErrors() }} type="reset">Reset</button>        
-                    <button className="btn btn-primary m-2" type="submit">Submit</button>        
+                { update ?
+                    <>
+                        <button className="btn btn-outline-primary m-2" onClick={onCancel} type="reset">Cancel</button>        
+                        <button className="btn btn-primary m-2" type="submit">Submit</button>        
+                    </>
+                    :
+                    <button className="btn btn-primary m-2" onClick={() => setUpdate(true)} type="button">Update</button>        
+                }
                 </div>
             </form>
         </div>
