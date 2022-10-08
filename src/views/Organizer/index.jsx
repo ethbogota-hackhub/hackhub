efa33b201fcdeaf72c6b6ab808504b6cddfafbb7
Juamez <juanmendezs.a@gmail.com>
2022-10-08 12:01:36 -0500

    Update form hooks and UI

--- src/views/Organizer/index.jsx
+++ src/views/Organizer/index.jsx
@@ -1,84 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import organizer from '../../assets/organizer.svg'
 
 
 const Organizer = () => {
-  const { register, setError, clearErrors, handleSubmit, formState: { errors } } = useForm()
+  const [ paymentMethod ] = useState([
+    'Polygon (10.08)',
+    'Optimism (12)',
+    'Ethereum (0.002)',
+    'DAI (22)',
+  ])
+  const { register, watch, handleSubmit, formState: { errors } } = useForm()
+  
   const onSubmit = data => {
     try {
-      console.log(data)
+      console.log(watch(data))
     } catch (err) {
       console.error(err)
     }
   }
 
-
   return (
     <div className='container-fluid'>
       <h1 className='text-center mb-5'>Get to know your event</h1>
       <img className='w-25 rounded mx-auto d-block' src={organizer} alt="" />
-      <form onSubmit={ handleSubmit(onSubmit) } action="" id="" className='d-flex flex-column align-items-center shadow mx-5'>
+      <form onSubmit={ handleSubmit(onSubmit) } className='d-flex flex-column align-items-center shadow mx-5'>
         <div className='d-flex flex-column w-75 my-4 form-floating'>
-            <input {...register("organizerName", { required: true })} className={ `border border-primary border-1 rounded shadow-sm form-control ${ errors.userDescription ? 'is-invalid' : '' }` } type="text" name="floatingInput" id="floatingInput" aria-label='' placeholder='Organizer Name' />
+            <input type="text" name="floatingInput" {...register("organizerName", { required: true })} className={ `border border-primary border-1 rounded shadow-sm form-control ${ errors.organizerName ? 'is-invalid' && 'border-danger' : 'border-success' }` } id="floatingInput" aria-label='' placeholder='Organizer Name' />
             <label htmlFor="floatingInput">Organizer Name</label>
-          </div>
+        </div>  
+
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventName", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingEvent" id="floatingEvent" aria-label='' placeholder='Event Name'/>
+            <input type="text" name="floatingEvent" {...register("eventName", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventName ? 'is-invalid' && 'border-danger' : 'border-success' }`}  id="floatingEvent" aria-label='' placeholder='Event Name'/>
             <label htmlFor="floatingEvent">Event Name</label>
         </div>
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventDescription", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingDesc" id="floatingDesc" aria-label='' placeholder='Event Description' />
+            <input type="text" name="floatingDesc" {...register("eventDescription", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventDescription ? 'is-invalid' && 'border-danger' : 'border-success' }`} id="floatingDesc" aria-label='' placeholder='Event Description' />
             <label htmlFor="floatingDesc">Event Description</label>
         </div>
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventLocation", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingLocation" id="floatingLocation" aria-label='' placeholder='Event Location' />
+            <input type="text" name="floatingLocation" {...register("eventLocation", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventLocation ? 'is-invalid' && 'border-danger' : 'border-success' }` } id="floatingLocation" aria-label='' placeholder='Event Location' />
             <label htmlFor="floatingLocation">Event Location</label>
         </div>
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventDateAndTime", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingDate&Time" id="floatingDate&Time" aria-label='' placeholder='Event Date and Time' />
+            <input type="text" name="floatingDate&Time" {...register("eventDateAndTime", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventDateAndTime ? 'is-invalid' && 'border-danger' : 'border-success' }`} id="floatingDate&Time" aria-label='' placeholder='Event Date and Time' />
             <label htmlFor="floatingDate&Time">Event Date and time</label>
         </div>
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventCover", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingCoverImg" id="floatingCoverImg" aria-label='' placeholder='Event Cover Image' />
+            <input type="text" name="floatingCoverImg" {...register("eventCover", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventCover ? 'is-invalid'  && 'border-danger' : 'border-success' }`} id="floatingCoverImg" aria-label='' placeholder='Event Cover Image' />
             <label htmlFor="floatingCoverImg">Event Cover Image</label>
         </div>
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventType", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingType" id="floatingType" aria-label='' placeholder='Event Type' />
+            <input type="text" name="floatingType" {...register("eventType", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventType ? 'is-invalid' && 'border-danger' : 'border-success' }`} id="floatingType" aria-label='' placeholder='Event Type' />
             <label htmlFor="floatingType">Event Type</label>
         </div>
         <div className='d-flex flex-column w-75 mb-4 form-floating'>
-            <input {...register("eventTopic", { required: true })} className='border border-primary border-1 rounded shadow-sm form-control' type="text" name="floatingTopic" id="floatingTopic" aria-label='' placeholder='Event Topic' />
+            <input type="text" name="floatingTopic" {...register("eventTopic", { required: true })} className={`border border-primary border-1 rounded shadow-sm form-control ${ errors.eventTopic ? 'is-invalid' && 'border-danger' : 'border-success' }`} id="floatingTopic" aria-label='' placeholder='Event Topic' />
             <label htmlFor="floatingTopic">Event Topic</label>
         </div>
         <div className='w-75 mb-4'>
             <label htmlFor="">Event Options</label>
             <div className='d-flex justify-content-evenly'>
               <div>
-                <input {...register("paidEvents", { required: true })} type="checkbox" name="paid" id="paid" aria-label='' />
+                <input type="checkbox" name="paid" {...register("paidEvents", { required: true})} id="paid" aria-label='' />
                 <label htmlFor="paid" className='px-2'>Paid Events</label>
               </div>
               <div>
-                <input {...register("freeEvents", { required: true })} type="checkbox" name="free" id="free" aria-label='' />
+                <input type="checkbox" name="free" {...register("freeEvents", { required: true})} id="free" aria-label='' />
                 <label htmlFor="free" className='px-2'>Free Events</label>
               </div>
             </div>
         </div>
-          <div className='mb-4 w-75'>
-            <label htmlFor="">Ticket Price</label>
-            {/* <input type="radio" name="" id="" aria-label='' /> */}
+        <div className='mb-4 w-75'>
+          <p className="mb-1">Token Prices</p>
+          <select name="option5"
+              type="select"
+              {...register('paymentMethod', { required: true })} 
+              className={ `form-select ${ errors.paymentMethod ? 'is-invalid' : ''  }` }>
+              {  paymentMethod.map((opt, index) => <option key={ `${opt}${index}`} value={ opt }>{ opt }</option>) }
+          </select>
           </div>
           <div className='mb-4 w-75'>
             <label htmlFor="">Ticket Options</label>
             <div>
-              <input {...register("limitedTickets", { required: true })} type="checkbox" name="limited" id="limited" aria-label='' />
+              <input type="checkbox" name="limited" {...register("limitedTickets", { required: true })} id="limited" aria-label='' />
               <label htmlFor="limited" className='px-2'>Limited Tickets</label>
             </div>
           </div>
             <div className='w-75 mb-4'>
               <h3>Terms and conditions</h3>
               <div>
-                <input {...register("termsAndConditions", { required: true })} type="checkbox" className='red' name="terms" id="terms" aria-label='' />
+                <input type="checkbox" name="terms" {...register("termsAndConditions")}  className='red' id="terms" aria-label='' />
                 <label htmlFor="terms" className='px-2'>By creating and event, I agree to the policies and terms of use.</label>
               </div>
             </div>
