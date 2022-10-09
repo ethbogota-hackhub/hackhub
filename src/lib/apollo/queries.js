2a8e852028cb93dcb5327c1bbfae97bdd6f7c469
Wilfredo Alcala <alcalawilfre@gmail.com>
2022-10-09 00:02:08 -0500

    add profiles query in attendees view

--- src/lib/apollo/queries.js
+++ src/lib/apollo/queries.js
@@ -112,3 +112,95 @@ query Profiles ($ownedBy: EthereumAddress!) {
   }
 }
 `;
+
+
+export const PROFILES_BY_HANDLES = gql`
+query Profiles {
+  profiles(request: { profileIds: ["0x06", "0x15", "0x16", "0x18", "0x49e3", "0x49fd", "0x0588", "0x0282", "0x0528", "0x41cd"], limit: 15 }) {
+    items {
+      id
+      name
+      bio
+      attributes {
+        displayType
+        traitType
+        key
+        value
+      }
+      followNftAddress
+      metadata
+      isDefault
+      picture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      handle
+      coverPicture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      ownedBy
+      dispatcher {
+        address
+        canUseRelay
+      }
+      stats {
+        totalFollowers
+        totalFollowing
+        totalPosts
+        totalComments
+        totalMirrors
+        totalPublications
+        totalCollects
+      }
+      followModule {
+        ... on FeeFollowModuleSettings {
+          type
+          amount {
+            asset {
+              symbol
+              name
+              decimals
+              address
+            }
+            value
+          }
+          recipient
+        }
+        ... on ProfileFollowModuleSettings {
+         type
+        }
+        ... on RevertFollowModuleSettings {
+         type
+        }
+      }
+    }
+    pageInfo {
+      prev
+      next
+      totalCount
+    }
+  }
+}`;
--- src/views/Attendees.jsx
+++ src/views/Attendees.jsx
@@ -1,28 +1,37 @@
-import { useState } from 'react';
-// import { useAccount } from 'wagmi';
+import { useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
 import Footer from '../components/Footer';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 // import { useForm } from 'react-hook-form';
 import { scrollToTop } from "../common/utils";
 import { ATTENDEES_CONST, SKILLS_CONST, ROLE_CONST } from '../const';
+import { useLazyQuery, useQuery } from '@apollo/client';
+import { PROFILES_BY_HANDLES } from '../lib/apollo/queries';
 
 const Attendees = () => {
   const [error, setError] = useState(null);
   const [pagination, setPagination] = useState(8);
-  const [personType, setPersonType] = useState([]); 
-  const [mainLanguage, setMainLanguage] = useState([]); 
+  const [personType, setPersonType] = useState([]);
+  const [mainLanguage, setMainLanguage] = useState([]);
+  const [getAttendees, { }] = useLazyQuery(PROFILES_BY_HANDLES);
 
-  // const { address, connector } = useAccount();
-  const [attendees,] = useState(ATTENDEES_CONST);
+  const [attendees, setAttendees] = useState([]);
+
+  useEffect(() => {
+    getAttendees().then((res) => {
+      console.log(res.data.profiles.items);
+      setAttendees(res.data.profiles.items);
+    });
+  }, []);
 
   const onSubmit = () => {
     try {
       setError(null);
 
-      if (mainLanguage?.length === 0 && personType?.length === 0) 
+      if (mainLanguage?.length === 0 && personType?.length === 0)
         return setError('You must select at least one value to do a search');
-        
+
       const skills = mainLanguage.map(lang => lang.value);
       const roles = personType.map(pers => pers.value);
 
@@ -55,7 +64,7 @@ const Attendees = () => {
       });
 
       if (filteredAttendees.length === 0) return setError('No results found');
-   
+
       console.log(filteredAttendees)
     } catch (e) {
       console.log(e);
@@ -63,13 +72,13 @@ const Attendees = () => {
   }
 
   const clearValues = () => { setError(null); setPagination(8); }
-  
+
   return (
     <div className='container-fluid'>
       <div className="row mx-0">
         <div className="col-12 col-md-8">
-            <h1 className="home-title mb-0 ps-3">Find your teammate.</h1>
-            <h1 className="home-title mt-2 ps-3 fw-bold">Build your team.</h1>
+          <h1 className="home-title mb-0 ps-3">Find your teammate.</h1>
+          <h1 className="home-title mt-2 ps-3 fw-bold">Build your team.</h1>
         </div>
 
         <div className="col-12 col-md-4 pb-5 d-flex justify-content-center align-items-center">
@@ -84,23 +93,23 @@ const Attendees = () => {
           <Select
             isMulti
             onChange={(e) => { setError(null); setMainLanguage(e); }}
-            components={ makeAnimated() } 
-            options={ SKILLS_CONST } />
+            components={makeAnimated()}
+            options={SKILLS_CONST} />
         </div>
 
         <div className='py-2 m-0'>
-          <p className="mb-1">What type of individual are you looking for?</p>          
+          <p className="mb-1">What type of individual are you looking for?</p>
           <Select
             isMulti
             onChange={(e) => { setError(null); setPersonType(e) }}
-            components={ makeAnimated() } 
-            options={ ROLE_CONST } />
+            components={makeAnimated()}
+            options={ROLE_CONST} />
         </div>
 
         {error && <div className='m-2 text-center text-danger'>{error}</div>}
 
         <div className='d-flex justify-content-center align-items-center mt-2'>
-          <button type='button' onClick={ onSubmit } className='btn rounded-pill btn-join-us btn-primary m-2'>Submit</button>
+          <button type='button' onClick={onSubmit} className='btn rounded-pill btn-join-us btn-primary m-2'>Submit</button>
         </div>
       </div>
 
